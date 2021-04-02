4d9fab36923b3ca9eba4b176d21526de666ac6f8
salamidrus <idrussalam95gmail.com>
2021-04-02 09:58:38 +0700

    Add Profile

---
+++ models/profile.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const profileSchema = new Schema(
+  {
+    age: {
+      type: Number,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const profile = mongoose.model("Profile", profileSchema);
+
+module.exports = profile;
